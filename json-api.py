b61d8dcab2047220cc3a76c1b24e2f2865836d2d
Timo <timo.fischer@protonmail.com>
2022-11-02 21:10:04 +0100

    commit

--- json-api.py
+++ json-api.py
@@ -1,9 +1,15 @@
 import json
 import requests
 
-url = 'https://api.punkapi.com/v2/beers'
+url = 'https://api.punkapi.com/v2/beers?food=pie'
 
 r = requests.get(url)
 
 data = json.loads(r.text)
+
+#print(data[0]['name'], data[0]['tagline'], data[0]['abv'])
+
+print(len(data))
+
+
 
