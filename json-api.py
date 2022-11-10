d333af5ab6b18d4b19e8f61fa5fb8eca76fbd0aa
Timo <timo.fischer@protonmail.com>
2022-11-10 09:56:36 +0100

    added new variables

--- json-api.py
+++ json-api.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from random import randint
 
 food_choice = input('Please enter your dinner choice: ')
 
@@ -21,6 +22,13 @@ for beer in data:
         'tagline': tagline
     }
     beer_list.append(beer_item)
+    
+value = randint(0, len(beer_list))
+
+try_this = beer_list[value]
+
+try_name = try_this['name']
+try_tagline = try_this['tagline']
 
 
 
