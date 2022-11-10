a87128f732a4ec5a68a823d4b638e257c0b24738
Timo <timo.fischer@protonmail.com>
2022-11-10 10:04:32 +0100

    added list

--- json-api.py
+++ json-api.py
@@ -1,15 +1,36 @@
 import json
 import requests
+from random import randint
 
-url = 'https://api.punkapi.com/v2/beers?food=pie'
+food_choice = input('Please enter your dinner choice: ')
+
+url = f'https://api.punkapi.com/v2/beers?food={food_choice}'
 
 r = requests.get(url)
 
 data = json.loads(r.text)
 
-print(data[0]['name'], data[0]['tagline'], data[0]['abv'])
+beer_list = []
 
-#print(len(data))
+for beer in data:
+    name = beer['name']
+    tagline = beer['tagline']
+    
+
+    beer_item = {
+        'name': name,
+        'tagline': tagline
+    }
+    beer_list.append(beer_item)
+    
+value = randint(0, len(beer_list))
+
+try_this = beer_list[value]
+
+try_name = try_this['name']
+try_tagline = try_this['tagline']
+
+print(f'You shuld try {try_name}, {try_tagline}')
 
 
 
